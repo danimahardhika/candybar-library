ff326c95a8f7f347eb2d4142d9578c9f7627af01
Dani Mahardhika <danimahardhika112@gmail.com>
2016-11-07 20:06:55 +0700

    Fix license

--- core/src/main/java/com/dm/material/dashboard/candybar/preferences/Preferences.java
+++ core/src/main/java/com/dm/material/dashboard/candybar/preferences/Preferences.java
@@ -10,17 +10,17 @@ import com.dm.material.dashboard.candybar.helpers.PreferencesHelper;
  *
  * Copyright (c) 2014-present Dani Mahardhika
  *
- * Licensed under the Apache LicenseHelper, Version 2.0 (the "LicenseHelper");
- * you may not use this file except in compliance with the LicenseHelper.
- * You may obtain a copy of the LicenseHelper at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the LicenseHelper is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the LicenseHelper for the specific language governing permissions and
- * limitations under the LicenseHelper.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 public class Preferences {
